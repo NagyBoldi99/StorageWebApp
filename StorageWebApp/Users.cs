e112e75c1b75609b4fd5c807ef49d47d7aad8aed
NagyBoldi99 <boboldi1999@gmail.com>
2024-12-17 20:42:49 +0100

    Initial commit

---
+++ StorageWebApp/Users.cs
@@ -0,0 +1,13 @@
+﻿public class User
+{
+    public int Id { get; set; } // Elsődleges kulcs
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+
+    // Validálási metódus
+    public bool IsValid()
+    {
+        return !string.IsNullOrEmpty(Name) && !string.IsNullOrEmpty(Email) && !string.IsNullOrEmpty(Password);
+    }
+}
